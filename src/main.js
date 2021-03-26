def88956cc475dec4feb79a6fa76af241adbfaf9
chuni-lin <william811566@gmail.com>
2021-03-26 16:36:32 +0800

    feat: install jQuery

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
 
 Vue.config.productionTip = false
 
