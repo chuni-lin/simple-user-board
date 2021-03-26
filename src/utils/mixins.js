43851ab06f91ffa3b384d64f259d1befcc31d445
chuni-lin <william811566@gmail.com>
2021-03-26 18:29:14 +0800

    feat: datetime

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,3 +1,13 @@
+import { formatDistanceToNow, parseISO } from 'date-fns'
+
+export const fromNowFilter = {
+  filters: {
+    fromNow (datetime) {
+      return datetime ? formatDistanceToNow(parseISO(datetime)) : '-'
+    }
+  }
+}
+
 export const getEmailAccount = {
   filters: {
     getEmailAccount (email) {
